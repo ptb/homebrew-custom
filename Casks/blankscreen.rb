04125f5c01d4c443e1ec5c8b7fbf56e61381337b
Peter T Bosse II <ptb@ioutime.com>
2015-06-19 08:45:51 -0400

    Update BlankScreen, Composer, and Synergy

--- Casks/blankscreen.rb
+++ Casks/blankscreen.rb
@@ -6,7 +6,7 @@ cask :v1 => 'blankscreen' do
 
   url 'http://www.wurst-wasser.net/wiki/images/3/30/BlankScreenSaverApp_0200.zip'
   sha256 'a05d46b466ed44cca77ddbd5d7ea2f31baed81fe28dc1859ccd365d76ade1a16'
-  screen_saver 'BlankScreen.saver'
+  screen_saver 'BlankScreen Saver Folder/BlankScreen.saver'
 
   license :oss
 end
--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -6,7 +6,7 @@ cask :v1 => 'synergy' do
 
   url 'https://wincent.com/l/synergy6'
   sha256 '8d8de537eb192565af02557c4f2ab1bbb2b5fa34e97ab4db62385de0798bba71'
-  app 'Synergy Preferences.app'
+  app 'Synergy 6.0a4 (96eed54)/Synergy Preferences.app'
 
   license :commercial
 end
