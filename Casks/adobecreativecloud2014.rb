97ed66c8c45d1128a6679ce5f8dd039e5b368a8c
Peter T Bosse II <ptb@ioutime.com>
2015-06-21 07:53:31 -0400

    Update adobecreativecloud.rb

--- Casks/adobecreativecloud2014.rb
+++ Casks/adobecreativecloud2014.rb
@@ -4,7 +4,7 @@ cask :v1 => 'adobecreativecloud2014' do
 
   homepage 'https://creative.adobe.com/products/creative-cloud'
 
-  url 'http://depot.ptb2.me/Software/adobe.com/Creative%20Cloud%2FAdobe%20Creative%20Cloud%202014.pkg'
+  url 'http://depot.ptb2.me/Software/adobe.com/Creative%20Cloud/Adobe%20Creative%20Cloud%202014.pkg'
   sha256 '3c419be9ec4ecc8c281850ab033528a8a12411adb05ffec1d966b98f845476ff'
   pkg 'Adobe Creative Cloud 2014.pkg'
 
