c985ca9c2934651ef790ace63164b3c7e65e370f
Peter T Bosse II <ptb@ioutime.com>
2015-07-13 18:45:51 -0400

    Update text-to-speech-allison for 10.11

--- Casks/text-to-speech-allison.rb
+++ Casks/text-to-speech-allison.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'text-to-speech-allison' do
   name 'Text to Speech: Allison'
-  version '4.0.1'
+  version '5.0.3'
 
   homepage 'https://support.apple.com/kb/PH18734'
 
   url 'https://setup.ptb2.me/Software/apple.com/OS%20X/Voices/Text-to-Speech%20Allison.pkg'
-  sha256 'c8f54f2a3a8d21d4113544edb9e549a08df5bf599c7d822b419df7358efd3647'
+  sha256 'dc02bdc137ad01ddc894fe65c0f650a57bbbb57f6985f5eb38effa2d87858122'
   pkg 'Text-to-Speech Allison.pkg'
 
   license :commercial
