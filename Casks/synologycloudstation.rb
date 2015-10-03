a9d335db3bcbfd2a6dcb5ad8570a49db65d3853b
Peter T Bosse II <ptb@ioutime.com>
2015-10-03 10:43:49 -0400

    Update Synology Cloud Station artifact

--- Casks/synologycloudstation.rb
+++ Casks/synologycloudstation.rb
@@ -6,7 +6,7 @@ cask :v1 => 'synologycloudstation' do
 
   url 'https://setup/Software/synology.com/Synology%20Cloud%20Station/Synology%20Cloud%20Station%202015.09.14.dmg'
   sha256 'aa544573ca9f7d3cb6693cba2406901a8b5e241eba0ed12762346c2f31b7c0cd'
-  app 'Synology Cloud Station.app'
+  pkg 'synology-cloud-station-3484.pkg'
 
   license :commercial
 end
