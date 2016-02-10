d41f51cf9d8b294e31aad04b9749c9d1c7e2b550
ptb <ptb@ioutime.com>
2016-02-10 14:56:03 -0500

    Create pcalc-3.rb

---
+++ Casks/pcalc-3.rb
@@ -0,0 +1,11 @@
+cask 'pcalc-3' do
+  version '3.9'
+  sha256 '3d0b3c034c944e2d007f95d201fd999d3c98c29fc7e65105bdaeb04c3b7b8310'
+
+  url 'http://s3.amazonaws.com/tlasystems/PCalc-3.9.dmg'
+  name 'PCalc 3'
+  homepage 'http://www.pcalc.com/english/download.html'
+  license :commercial
+
+  app 'PCalc.app'
+end
