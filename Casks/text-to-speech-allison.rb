2c10cce32fde2cf8c8f93e00ece54dd0ff78e8f3
ptb <ptb@ioutime.com>
2016-02-10 14:59:48 -0500

    Update text-to-speech-allison.rb

--- Casks/text-to-speech-allison.rb
+++ Casks/text-to-speech-allison.rb
@@ -1,12 +1,15 @@
-cask :v1 => 'text-to-speech-allison' do
-  name 'Text to Speech: Allison'
+cask 'text-to-speech-allison' do
   version '5.0.7'
-
-  homepage 'https://support.apple.com/kb/PH18734'
-
-  url 'https://setup/Software/apple.com/OS%20X/Voices/10.11/Text-to-Speech%20Allison.pkg'
   sha256 '8036465369c8c0424956ed3f5060e99784ce499c38e2e6b1b67ffb875df688d0'
-  pkg 'Text-to-Speech Allison.pkg'
 
+  # curl --compressed --location --silent \
+  #   "https://swscan.apple.com/content/catalogs/others/index-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog.gz" \
+  #   | sed -n -e "s/^.*<string>\(.*MultiLingualVoice_en_US_allison.pkg\).*/\1/p"
+
+  url 'http://swcdn.apple.com/content/downloads/41/27/031-26106/yn6a4cxyv9t5wv5bnzy6qkmbfkpvgosrt2/MultiLingualVoice_en_US_allison.pkg'
+  name 'Text to Speech: Allison'
+  homepage 'https://support.apple.com/kb/PH18734'
   license :commercial
+
+  pkg 'MultiLingualVoice_en_US_allison.pkg'
 end
