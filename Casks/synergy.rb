22eded4b2589366b8816d0d7785ebe3f93ca6b0e
ptb <ptb@ioutime.com>
2016-02-10 15:09:52 -0500

    Update synergy.rb

--- Casks/synergy.rb
+++ Casks/synergy.rb
@@ -1,12 +1,11 @@
-cask :v1 => 'synergy' do
-  name 'Synergy'
+cask 'synergy' do
   version '6.0a4'
-
-  homepage 'https://wincent.com/products/synergy'
-
-  url 'https://wincent.com/l/synergy6'
   sha256 '8d8de537eb192565af02557c4f2ab1bbb2b5fa34e97ab4db62385de0798bba71'
-  app 'Synergy 6.0a4 (96eed54)/Synergy Preferences.app'
 
+  url 'http://s3.wincent.com/synergy/releases/synergy-6.0a4.zip'
+  name 'Synergy'
+  homepage 'https://wincent.com/products/synergy'
   license :commercial
+
+  app 'Synergy 6.0a4 (96eed54)/Synergy Preferences.app'
 end
