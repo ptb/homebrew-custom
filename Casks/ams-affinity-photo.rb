1cd0df690a412b5a17623581b7fec62634d1d5a6
ptb <ptb@ioutime.com>
2016-02-11 17:40:42 -0500

    Create ams-affinity-photo.rb

---
+++ Casks/ams-affinity-photo.rb
@@ -0,0 +1,11 @@
+cask 'ams-affinity-photo' do
+  version '1.4.1'
+  sha256 'abe7f234fc9579aff02c4e72b9ffe492754fe8a4fd45989aba0d5cc3878e337b'
+
+  url 'file:///Volumes/Storage/Software/serif.com/mas-affinity-photo-1.4.1.pkg'
+  name 'Affinity Photo'
+  homepage 'https://affinity.serif.com/en-us/photo/'
+  license :commercial
+
+  pkg 'mas-affinity-photo-1.4.1.pkg'
+end
