f1530ac3de5ea58251ccfd5f3b5a162ab3318809
ptb <ptb@ioutime.com>
2016-02-13 10:23:48 -0500

    Create ipmenulet.rb

---
+++ Casks/ipmenulet.rb
@@ -0,0 +1,11 @@
+cask 'ipmenulet' do
+  version '1.2.1'
+  sha256 '0a7e08e866bb95c234ecf6f3035946909d3c5bfa47f395053ff8ff99a5721769'
+
+  url "https://github.com/mcandre/IPMenulet/releases/download/v#{version}/IPMenulet-#{version}.dmg"
+  name 'IPMenulet'
+  homepage 'http://www.yellosoft.us/ipmenulet'
+  license :bsd
+
+  app 'IPMenulet.app'
+end
