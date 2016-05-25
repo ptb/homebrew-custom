72c51af37718ab734349404b844f8c5ecf515207
ptb <ptb@ioutime.com>
2016-05-25 07:41:52 -0400

    Update tune4mac.rb

--- Casks/tune4mac.rb
+++ Casks/tune4mac.rb
@@ -1,10 +1,10 @@
 cask 'tune4mac' do
-  version '4.1.5'
-  sha256 'c10653aa3d1e444623eaf7dc295a0f46aa1ac9561f83b81243ea9a643a2a825b'
+  version '4.1.7'
+  sha256 'b4937c9bfa2ff6ffac03081503ee786fb871c77a51ad19eed7fa092ea22f0fcd'
 
   url 'http://www.tune4mac.com/download/mac-itunes-video-converter-platinum.dmg'
   name 'Tune4mac iTunes Video Converter'
-  homepage 'http://www.tune4mac.com/mac-itunes-video-converter/'
+  homepage 'http://www.tune4mac.com/mac-itunes-video-converter-platinum/'
   license :commercial
 
   app 'Tune4mac iTunes Video Converter Platinum.app'
