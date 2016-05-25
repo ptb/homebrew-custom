a13dd54b19afdda947c8df1bb71a533766c9b2ed
ptb <ptb@ioutime.com>
2016-05-25 07:33:22 -0400

    Update mas-keynote.rb

--- Casks/mas-keynote.rb
+++ Casks/mas-keynote.rb
@@ -1,11 +1,11 @@
 cask 'mas-keynote' do
-  version '6.6.1'
-  sha256 '43b0cf5505cbc712569959f0e072a75bae561eee16e17eb0e6813c6802b2da86'
+  version '6.6.2'
+  sha256 '9e1f233acf4929e8af85db688974cabb1f29ad69550887858af82997dfee5082'
 
-  url 'file:///Volumes/Storage/Software/apple.com/mas-keynote-6.6.1.pkg'
+  url 'file:///Volumes/Storage/Software/apple.com/mas-keynote-6.6.2.pkg'
   name 'Keynote'
   homepage 'http://www.apple.com/mac/keynote/'
   license :commercial
 
-  pkg 'mas-keynote-6.6.1.pkg'
+  pkg 'mas-keynote-6.6.2.pkg'
 end
