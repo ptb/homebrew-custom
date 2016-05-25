8c5a0c3ea5160b61a31d1f1020caae2227e03073
ptb <ptb@ioutime.com>
2016-05-25 07:27:32 -0400

    Update mas-icon-slate.rb

--- Casks/mas-icon-slate.rb
+++ Casks/mas-icon-slate.rb
@@ -1,11 +1,11 @@
 cask 'mas-icon-slate' do
-  version '4.4.5'
-  sha256 '547f80cb357dd01b57e9a8cbca54da5814d37522364664d7e70498d3718f8a5a'
+  version '4.4.7'
+  sha256 'f17d638fa77179919066a1563e723179edea008b90a1dfbe72839d2df0a1d19c'
 
-  url 'file:///Volumes/Storage/Software/kodlian.com/mas-icon-slate-4.4.5.pkg'
+  url 'file:///Volumes/Storage/Software/kodlian.com/mas-icon-slate-4.4.7.pkg'
   name 'Icon Slate'
   homepage 'http://www.kodlian.com/apps/icon-slate'
   license :commercial
 
-  pkg 'mas-icon-slate-4.4.5.pkg'
+  pkg 'mas-icon-slate-4.4.7.pkg'
 end
