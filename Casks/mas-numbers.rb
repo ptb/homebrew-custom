bdfab355e4246d7ffedf8878519311e3be0dde40
ptb <ptb@ioutime.com>
2016-05-25 07:33:58 -0400

    Update mas-numbers.rb

--- Casks/mas-numbers.rb
+++ Casks/mas-numbers.rb
@@ -1,11 +1,11 @@
 cask 'mas-numbers' do
-  version '3.6.1'
-  sha256 '824a1de76b31db51e920231fd4227e438a47989e7f49d20efaf9e95d14161465'
+  version '3.6.2'
+  sha256 'd39ee37c06d91a8f20fff8f49eb8c22eeb4e795cddf47af052243273461d88fb'
 
-  url 'file:///Volumes/Storage/Software/apple.com/mas-numbers-3.6.1.pkg'
+  url 'file:///Volumes/Storage/Software/apple.com/mas-numbers-3.6.2.pkg'
   name 'Numbers'
   homepage 'http://www.apple.com/mac/numbers/'
   license :commercial
 
-  pkg 'mas-numbers-3.6.1.pkg'
+  pkg 'mas-numbers-3.6.2.pkg'
 end
