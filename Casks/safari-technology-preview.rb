ff7ca7eff1e792e63a24d340b20b6ccfa0279438
ptb <ptb@ioutime.com>
2017-07-05 16:36:56 -0400

    Update safari-technology-preview.rb

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -2,7 +2,7 @@ cask 'safari-technology-preview' do
   version :latest
   sha256 :no_check
 
-  url 'https://secure-appldnld.apple.com/STP/031-77729-20160915-743EF09E-7A1A-11E6-B739-C11634D2D062/SafariTechnologyPreview.dmg'
+  url 'https://secure-appldnld.apple.com/STP/091-17778-20170628-3F7E556A-5B70-11E7-A994-CCEF7F942F55/SafariTechnologyPreview.dmg'
   name 'Safari Technology Preview'
   homepage 'https://developer.apple.com/safari/technology-preview/'
   license :gratis
