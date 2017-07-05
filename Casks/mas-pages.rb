1bc18dcd2fc44202093a0829ffc5e2c1ffea60a0
ptb <ptb@ioutime.com>
2017-07-05 16:41:59 -0400

    Update mas-pages.rb

--- Casks/mas-pages.rb
+++ Casks/mas-pages.rb
@@ -1,11 +1,11 @@
 cask 'mas-pages' do
-  version '6.0'
-  sha256 '3a4dda7f3c88fda16f225d2d259397e5a5acdeb0e68f4afe8cdcf87dfdeca743'
+  version '6.2'
+  sha256 'e817e62a593577d57c6a677937310498d7c32da121f50965daec35b228ad31ca'
 
-  url 'file:///Volumes/Storage/Software/apple.com/mas-pages-6.0.pkg'
+  url 'file:///Volumes/Storage/Software/apple.com/mas-pages-6.2.pkg'
   name 'Pages'
   homepage 'http://www.apple.com/mac/pages/'
   license :commercial
 
-  pkg 'mas-pages-6.0.pkg'
+  pkg 'mas-pages-6.2.pkg'
 end
