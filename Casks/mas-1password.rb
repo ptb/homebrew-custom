f5064663689150a88c614d4f75627d82f82f7ebe
ptb <ptb@ioutime.com>
2017-07-05 16:39:39 -0400

    Update mas-1password.rb

--- Casks/mas-1password.rb
+++ Casks/mas-1password.rb
@@ -1,11 +1,11 @@
 cask 'mas-1password' do
-  version '6.3.3'
-  sha256 '8bae1f74205b47924212f4257b6b4e4e0ce57fb5743fb83fc36e2d585bf4601f'
+  version '6.7'
+  sha256 '0def316594ab7e7dd1d9dbd4d1d4229cb816a1eec51092c210429b3ae5816d2d'
 
-  url 'file:///Volumes/Storage/Software/agilebits.com/mas-1password-6.3.3.pkg'
+  url 'file:///Volumes/Storage/Software/agilebits.com/mas-1password-6.7.pkg'
   name '1Password'
   homepage 'https://agilebits.com/onepassword/mac'
   license :commercial
 
-  pkg 'mas-1password-6.3.3.pkg'
+  pkg 'mas-1password-6.7.pkg'
 end
