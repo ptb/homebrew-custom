df5dfd80ac76734fbdf87a96dadd9db122c920b8
ptb <ptb@ioutime.com>
2017-07-05 16:40:40 -0400

    Update mas-keynote.rb

--- Casks/mas-keynote.rb
+++ Casks/mas-keynote.rb
@@ -1,11 +1,11 @@
 cask 'mas-keynote' do
-  version '7.0'
-  sha256 '270c4e568cbb4bb8d78dbd8e36be3b4eacf98583e7a829bc300f0a68f5c339cd'
+  version '7.2'
+  sha256 'b062e3f7a8e68578b7dd9763ae7239b963803e68aeb6948bc6cb66244c69a2f5'
 
-  url 'file:///Volumes/Storage/Software/apple.com/mas-keynote-7.0.pkg'
+  url 'file:///Volumes/Storage/Software/apple.com/mas-keynote-7.2.pkg'
   name 'Keynote'
   homepage 'http://www.apple.com/mac/keynote/'
   license :commercial
 
-  pkg 'mas-keynote-7.0.pkg'
+  pkg 'mas-keynote-7.2.pkg'
 end
