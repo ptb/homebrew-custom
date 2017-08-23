a703d28cd9685702629c53c713099833379cad25
Peter T Bosse II <ptb@ioutime.com>
2017-08-23 18:21:36 -0400

    Update ffmpeg.rb

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -11,9 +11,9 @@ class Ffmpeg < Formula
   end
 
   bottle do
-    sha256 "5a376b449da71f11aec2426ceab28c5797f429e8d1e29934541a7d57d4fed34f" => :sierra
-    sha256 "0d2fe136c30cc3a89746a1bfe67e4579766edda52f5f8f80b44aeafe0257e216" => :el_capitan
-    sha256 "57879c39bc53a7868ea5da360e4567c004062c581321a3707352151f12686768" => :yosemite
+    rebuild 1
+    root_url "file:///Volumes/Caches"
+    sha256 "b2043088d8e7336067e0a12b2b404bedd4c9c8e6c18f356b909086b2b260706c" => :high_sierra
   end
 
   option "with-chromaprint", "Enable the Chromaprint audio fingerprinting library"
