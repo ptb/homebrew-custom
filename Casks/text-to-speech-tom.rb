83a09b7dc32df5acc3c478e84bbfec269baeef49
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:30:15 -0400

    Update text-to-speech-tom.rb

--- Casks/text-to-speech-tom.rb
+++ Casks/text-to-speech-tom.rb
@@ -2,7 +2,7 @@ cask 'text-to-speech-tom' do
   version '7.0.2'
   sha256 '3d24dbfbc4240adef1176fc2ece8cc7ebae8704e0dcf93e3f42f7f3eb434cf85'
 
-  url 'file:///Volumes/Storage/Software/apple.com/text-to-speech-tom--7.0.2.pkg'
+  url 'file:///Volumes/Software/apple.com/text-to-speech-tom--7.0.2.pkg'
   name 'Text to Speech: Tom'
   homepage 'https://support.apple.com/kb/PH18734'
   license :commercial
