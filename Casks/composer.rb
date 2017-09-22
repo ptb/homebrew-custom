9e6b61ea3094250cbf15d898f77f7de60b041b3f
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:28:38 -0400

    Update composer.rb

--- Casks/composer.rb
+++ Casks/composer.rb
@@ -2,7 +2,7 @@ cask 'composer' do
   version '9.96'
   sha256 '0b1e9b689bd7863a20305ee4cc7eba54e360087621e14825e8033d09a05057a0'
 
-  url 'file:///Volumes/Storage/Software/jamfsoftware.com/composer-9.96.dmg'
+  url 'file:///Volumes/Software/jamfsoftware.com/composer-9.96.dmg'
   name 'JAMF Composer'
   homepage 'http://www.jamfsoftware.com/products/composer/'
   license :commercial
