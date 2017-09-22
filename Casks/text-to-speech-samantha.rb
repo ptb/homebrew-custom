d3f73882acc861abbdc270b4e30d485e804c720a
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:30:02 -0400

    Update text-to-speech-samantha.rb

--- Casks/text-to-speech-samantha.rb
+++ Casks/text-to-speech-samantha.rb
@@ -2,7 +2,7 @@ cask 'text-to-speech-samantha' do
   version '7.0.2'
   sha256 'bc32463607c8e7f7668fa4bb10cd32da8435f1e3f216c38cf70860736772a96d'
 
-  url 'file:///Volumes/Storage/Software/apple.com/text-to-speech-samantha--7.0.2.pkg'
+  url 'file:///Volumes/Software/apple.com/text-to-speech-samantha--7.0.2.pkg'
   name 'Text to Speech: Samantha'
   homepage 'https://support.apple.com/kb/PH18734'
   license :commercial
