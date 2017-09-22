dbadd18da1924b8aae3ab361bc35c95756561caf
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:29:49 -0400

    Update text-to-speech-allison.rb

--- Casks/text-to-speech-allison.rb
+++ Casks/text-to-speech-allison.rb
@@ -2,7 +2,7 @@ cask 'text-to-speech-allison' do
   version '7.0.2'
   sha256 '3550c7d6bd75b18844836a08ad01b8479ce9bc1677ded594bef39bbe49ef3515'
 
-  url 'file:///Volumes/Storage/Software/apple.com/text-to-speech-allison--7.0.2.pkg'
+  url 'file:///Volumes/Software/apple.com/text-to-speech-allison--7.0.2.pkg'
   name 'Text to Speech: Allison'
   homepage 'https://support.apple.com/kb/PH18734'
   license :commercial
