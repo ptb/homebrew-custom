e28e53988679c00bc9a3c383b2c62d9d75681a28
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:28:18 -0400

    Update adobe-creative-cloud-2014.rb

--- Casks/adobe-creative-cloud-2014.rb
+++ Casks/adobe-creative-cloud-2014.rb
@@ -2,7 +2,7 @@ cask 'adobe-creative-cloud-2014' do
   version '2015.4.30'
   sha256 '3c419be9ec4ecc8c281850ab033528a8a12411adb05ffec1d966b98f845476ff'
 
-  url 'file:///Volumes/Storage/Software/adobe.com/adobe-creative-cloud-2014.pkg'
+  url 'file:///Volumes/Software/adobe.com/adobe-creative-cloud-2014.pkg'
   name 'Adobe Creative Cloud 2014'
   homepage 'http://www.adobe.com/creativecloud.html'
   license :commercial
