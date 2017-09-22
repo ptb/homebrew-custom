6a45988d0e75d480348f6e35b2c0638dd1c3e855
Peter T Bosse II <ptb@ioutime.com>
2017-09-22 17:29:37 -0400

    Update text-to-speech-alex.rb

--- Casks/text-to-speech-alex.rb
+++ Casks/text-to-speech-alex.rb
@@ -2,7 +2,7 @@ cask 'text-to-speech-alex' do
   version '2.0.36'
   sha256 '4b98ce31af446746533e0fe3c2fc03e82fafe443ca1f96e00e9055679778bf4f'
 
-  url 'file:///Volumes/Storage/Software/apple.com/text-to-speech-alex--2.0.36.pkg'
+  url 'file:///Volumes/Software/apple.com/text-to-speech-alex--2.0.36.pkg'
   name 'Text to Speech: Alex'
   homepage 'https://support.apple.com/kb/PH18734'
   license :commercial
