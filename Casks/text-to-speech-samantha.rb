063516abb97738f1949343d00b5f089a0668ad72
Peter T Bosse II <ptb@ioutime.com>
2018-08-03 04:50:24 -0400

    Update text-to-speech-samantha.rb

--- Casks/text-to-speech-samantha.rb
+++ Casks/text-to-speech-samantha.rb
@@ -5,7 +5,6 @@ cask 'text-to-speech-samantha' do
   url 'file:///Volumes/Software/apple.com/text-to-speech-samantha--7.0.2.pkg'
   name 'Text to Speech: Samantha'
   homepage 'https://support.apple.com/kb/PH18734'
-  license :commercial
 
   pkg 'text-to-speech-samantha--7.0.2.pkg'
 end
